4ea03016c661e01cdb362aef6f13fcb46afe016e
DSkilton <32618413+DSkilton@users.noreply.github.com>
2020-07-09 10:48:57 +0100

    updated subject class

--- src/com/HeadFirstDesignPatterns/Main.java
+++ src/com/HeadFirstDesignPatterns/Main.java
@@ -1,5 +1,8 @@
 package com.HeadFirstDesignPatterns;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +12,34 @@ public class Main {
 //	    StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 
 	    weatherData.setMeasurements(80, 65, 30.4f);
-    }
+	}
+
+	class myMouseListener implements MouseListener {
+		public void mouseClicked(MouseEvent event){
+			System.out.println("Mouse click @ position x = " + event.getX() + " y = " + event.getY());
+		}
+
+		@Override
+		public void mousePressed(MouseEvent mouseEvent) {
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent mouseEvent) {
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent mouseEvent) {
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent mouseEvent) {
+
+		}
+	}
 }
+
+
+
--- src/com/HeadFirstDesignPatterns/Subject.java
+++ src/com/HeadFirstDesignPatterns/Subject.java
@@ -1,8 +1,21 @@
 package com.HeadFirstDesignPatterns;
 
 public interface Subject {
+    // takes observer as argument, they will be registered or removed
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
+
+    // will notify all observers to any state changes
     public void notifyObservers();
 }
 
+
+public interface Observer{
+    public void update  (float temp, float humidity, float pressure);
+
+}
+
+public interface DisplayElement {
+    public void display(); 
+
+}
